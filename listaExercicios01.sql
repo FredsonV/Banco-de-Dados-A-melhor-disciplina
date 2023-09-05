773d9d69c1dbedc110f14541c8ad640a1e1416a4
Fredson Vicente <fredsonvicente@yahoo.com>
2023-09-04 22:33:24 -0300

    atividade 3

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -4,3 +4,7 @@ select titulo from livros;
 --atividade 2
 select NOME from autores
 WHERE nascimento < '1900-01-01';
+
+--atividade 3
+select titulo.from livros 
+inner join autores on autores.nome = 'J.K. Rowling';
