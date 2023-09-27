71eedc985bf9e8a23a48603a5f26b4c488ee4cda
Fredson Vicente <fredsonvicente@yahoo.com>
2023-09-26 21:37:40 -0300

    atividade 2

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -9,3 +9,16 @@ END;
 DELIMITER ;
 
 call sp_ListarAutores()
+
+-- atividade 2
+DELIMITER //
+CREATE PROCEDURE sp_LivrosPorCategoria(in CategoriaNome VARCHAR(50))
+	BEGIN
+		select Livro.titulo
+		from Livro
+		inner join Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+	END;
+//
+DELIMITER ;
+
+call sp_LivrosPorCategoria('Autoajuda');
