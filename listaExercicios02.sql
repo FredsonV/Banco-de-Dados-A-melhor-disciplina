3b5cfaae2af06c57308cf3995753d2043eae9f84
Fredson Vicente <fredsonvicente@yahoo.com>
2023-09-26 22:39:56 -0300

    atividade 7

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -62,7 +62,7 @@ BEGIN
     where Ano_Publicacao <= ano;
 END;
 //
-DELIMITER ;
+DELIMITER;
 
 -- atividade 6 
 DELIMITER //
@@ -74,4 +74,32 @@ BEGIN
     where Categoria.Nome = categoriaNome;
 END;
 //
-DELIMITER ;
+DELIMITER;
+
+-- atividade 7
+DELIMITER //
+CREATE PROCEDURE sp_AdicionarLivro(
+    in titulo_livro varchar(100),
+    in editora_id int,
+    in ano_publicacao int,
+    in numero_paginas int,
+    in categoria_id int,
+    out mensagem varchar(100)
+)
+BEGIN
+    declare livro_real int;
+	select count(*) into livro_real
+    from Livro
+    where Titulo = titulo_livro;
+    if livro_real > 0 then
+        set mensagem = "ERRO!";
+    else
+        insert into Livro (Titulo, Editora_ID, Ano_Publicacao, Numero_Paginas, Categoria_ID)
+        values (titulo_livro, editora_id, ano_publicacao, numero_paginas, categoria_id);
+
+        set mensagem = "Livro adicionado";
+    end if;
+    select mensagem;
+END;
+//
+DELIMITER;
