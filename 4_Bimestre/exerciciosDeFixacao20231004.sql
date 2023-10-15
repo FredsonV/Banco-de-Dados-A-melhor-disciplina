c3521bc58201c86a96551cf83d953e9d0123103c
Fredson Vicente <fredsonvicente@yahoo.com>
2023-10-15 20:01:16 -0300

    atividade 2

--- 4_Bimestre/exerciciosDeFixacao20231004.sql
+++ 4_Bimestre/exerciciosDeFixacao20231004.sql
@@ -25,3 +25,42 @@ BEGIN
 END;
 
 SELECT total_livros_por_genero('Ficção Científica') AS total_ficcao_cientifica;
+
+
+--atividade 2
+CREATE FUNCTION listar_livros_por_autor(
+  primeiro_nome_autor_param VARCHAR(255),
+  ultimo_nome_autor_param VARCHAR(255)
+)
+RETURNS VARCHAR(255)
+BEGIN
+  DECLARE lista_titulos VARCHAR(1000);
+  SET lista_titulos = '';
+  
+  DECLARE cur CURSOR FOR  
+  SELECT Livro_ID FROM Livro_Autor
+  WHERE Autor_Primeiro_Nome = primeiro_nome_autor_param
+  AND Autor_Ultimo_Nome = ultimo_nome_autor_param;
+  
+  DECLARE Livro_id INT;
+  DECLARE done INT DEFAULT 0;
+  
+  DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+  
+  OPEN cur;
+  
+  read_loop: LOOP
+    FETCH cur INTO livro_id;
+    IF done THEN
+      LEAVE read_loop;
+    END IF;
+
+    SELECT Titulo INTO @titulo FROM Livro WHERE Livro_ID = livro_id;
+    
+    SET lista_titulos = CONCAT(lista_titulos, @titulo, ', ');
+  END LOOP;
+  
+  CLOSE cur;
+  
+  RETURN lista_titulos;
+END;
