b02bf6b10ceb40dcd897abd185fb16bd7b1312f5
Fredson Vicente <fredsonvicente@yahoo.com>
2023-10-25 21:34:20 -0300

    atividade 3

--- 4_Bimestre/exerciciosDeFixacao20231025.sql
+++ 4_Bimestre/exerciciosDeFixacao20231025.sql
@@ -11,3 +11,10 @@ BEFORE DELETE ON Clientes
 FOR EACH ROW
     INSERT INTO Auditoria (texto)
     VALUES ('Atenção!!! tentativa de exclusão na tabela Cliente ');
+
+--atividade 3
+CREATE TRIGGER cliente_updtae
+AFTER UPDATE ON Clientes
+FOR EACH ROW
+    INSERT INTO Auditoria (texto,)
+    VALUES ('O antigo nome: ' + OLD.Nome 'Foi alterado para:' + New.Nome)
